2b95a16cec5395ea3d375382b7a2d579ef840910
visaruth <visaruth.p@gmail.com>
2018-05-14 15:25:35 +0700

    Re construction

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+    
+    $fullURL = 'http://localhost/MegamindProject';
+    $header = 'libs/header.php';
+    $footer = 'libs/footer.php';
+    $assets = $fullURL.'/assets';
+    $imgDir = $assets.'/img';
+    $cssDir = $assets.'/css';
+    $jsDir = $assets.'/js';
+    $fontDir = $assets.'/font';
+?>
---
+++ libs/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Meg Mind ShoesFashion</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo $cssDir ?>/index.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $cssDir ?>/shopall.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $cssDir ?>/register.css">
+</head>
