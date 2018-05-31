ac03201d2bfad6207e39c8274be24d75e62b1fb4
visaruth <visaruth.p@gmail.com>
2018-05-31 20:58:32 +0700

    update sql

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2018 at 10:28 AM
+-- Generation Time: May 31, 2018 at 03:55 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -33,14 +33,6 @@ CREATE TABLE `cart` (
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`id`, `userid`, `productid`, `status`) VALUES
-(14, 5, 10, 2),
-(15, 5, 9, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -54,14 +46,6 @@ CREATE TABLE `shop` (
   `brand` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `img` varchar(200) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `shop`
---
-
-INSERT INTO `shop` (`id`, `name`, `price`, `brand`, `img`) VALUES
-(9, 'asdwa', 4254, 'fasdea', '9.jpg'),
-(10, 'asd', 4534, 'asfve', '10.png');
 
 -- --------------------------------------------------------
 
@@ -81,12 +65,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `type`) VALUES
-(1, 'meg', '202cb962ac59075b964b07152d234b70', 1),
-(2, 'meg', '202cb962ac59075b964b07152d234b70', 1),
-(3, 'meg', '202cb962ac59075b964b07152d234b70', 1),
-(4, 'sda', '202cb962ac59075b964b07152d234b70', 1),
-(5, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0),
-(6, 'asd', '65ded5353c5ee48d0b7d48c591b8f430', 1);
+(5, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0);
 
 --
 -- Indexes for dumped tables
@@ -118,12 +97,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `shop`
 --
 ALTER TABLE `shop`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user`
 --
